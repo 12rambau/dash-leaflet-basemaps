380a6b2c701aebeaa35279c76ba14407e970d9b1
Pierrick Rambaud <pierrick.rambaud@ldc.com>
2024-07-30 11:22:36 +0000

    feat: first implementation

--- dash_leaflet_basemaps/__init__.py
+++ dash_leaflet_basemaps/__init__.py
@@ -1,20 +1,32 @@
 """The init file of the package."""
+import dash_leaflet as dl
+
+from .basemaps import basemap_tiles
 
 __version__ = "0.0.0"
 __author__ = "Pierrick Rambaud"
 __email__ = "pierrick.rambaud49@gmail.com"
 
 
-class Hello:
-    """Hello world class."""
+class BasemapLayer(dl.TileLayer):
+    """A class to represent a basemap layer."""
 
-    msg = "hello world !"
-    "the message to print"
+    def __init__(self, name: str, **kwargs):
+        """Initialize the class.
 
-    def hello_world(self) -> str:
-        """Hello world demo method.
-
-        Returns:
-            the hello world string
+        Args:
+            name: The name of the basemap.
+            kwargs: any keyword arguments from dash-leaflet TileLayer class knowing that ``url``, ``id`` and ``attribution`` will be ignored.
         """
-        return self.msg
+        # check if the name exists
+        if name not in basemap_tiles:
+            raise ValueError(
+                f"Basemap {name} not found. Available basemaps are: [{', '.join(basemap_tiles.keys())}"
+            )
+
+        # get the basemap
+        kwargs["url"] = basemap_tiles[name].url
+        kwargs["id"] = basemap_tiles[name].id
+        kwargs["attribution"] = basemap_tiles[name].attribution
+        kwargs["max_zoom"] = kwargs.get("maxZoom", basemap_tiles[name].max_zoom)
+        super().__init__(kwargs)
